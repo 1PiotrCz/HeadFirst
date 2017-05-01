f3c0e0e9a63fcf3b0cc655751906992453b4621c
Piotr Czubkowski <piotrcz@o2.pl>
2017-05-01 23:51:09 +0200

    Exercise 1ab

---
+++ src/com/company/exercise1ab.java
@@ -0,0 +1,25 @@
+package com.company;
+
+/**
+ * Created by Piotr on 2017-05-01.
+ */
+public class exercise1ab {
+    public static void main(String[] args) {
+//        int x = 2;
+//        while (x < 10) {
+//            if (x > 3) {
+//                System.out.println("Wielkie X");
+//            }
+//        }
+
+//        int x = 5;
+//        while (x > 1) {
+//            x = x - 1;
+//            if (x < 3) {
+//                System.out.println("Malutkie x");
+//            }
+//        }
+    }
+}
+
+
